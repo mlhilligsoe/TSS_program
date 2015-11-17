db7387db08cf6d444aee7c47091e95ee4e257723
s114898 <s114898@student.dtu.dk>
2015-11-17 21:26:38 +0100

    Test

---
+++ MySQLtoExcel/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Mysql_to_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update tabel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(135, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(434, 324);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(23, 88);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(97, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Batch Order ID";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(23, 112);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(94, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Batch Storage";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(23, 136);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(96, 17);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Batch Quantity";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.Location = new System.Drawing.Point(23, 160);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(79, 17);
+            this.checkBox5.TabIndex = 6;
+            this.checkBox5.Text = "Batch Start";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Checked = true;
+            this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox6.Location = new System.Drawing.Point(23, 184);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(101, 17);
+            this.checkBox6.TabIndex = 7;
+            this.checkBox6.Text = "Batch Complete";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Checked = true;
+            this.checkBox7.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox7.Location = new System.Drawing.Point(23, 208);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(111, 17);
+            this.checkBox7.TabIndex = 8;
+            this.checkBox7.Text = "Number of Events";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Checked = true;
+            this.checkBox8.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox8.Location = new System.Drawing.Point(23, 232);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(101, 17);
+            this.checkBox8.TabIndex = 9;
+            this.checkBox8.Text = "Batch Comment";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Save to Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 332);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "Skriv fil navn her...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 370);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MySQLtoExcel/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace Mysql_to_Excel
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Server=127.0.0.1;Database=bosch;Uid=bosch;Pwd=12345;SslMode=None;Convert Zero Datetime=True;";
+        string query;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+
+            {
+                MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Database=bosch;Uid=bosch;Pwd=12345;SslMode=None;");
+                MessageBox.Show("connected with sql server");
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            query = "SELECT ";
+
+            query += "`batch_id`";
+
+
+            if (checkBox2.Checked == true)
+            {
+                query += ", `batch_order_id`";
+            }
+
+            if (checkBox3.Checked == true)
+            {
+                query += ", `batch_storage`";
+            }
+
+            if (checkBox4.Checked == true)
+            {
+                query += ", `batch_quantity`";
+            }
+
+            if (checkBox5.Checked == true)
+            {
+                query += ", `batch_start`";
+            }
+
+            if (checkBox6.Checked == true)
+            {
+                query += ", `batch_complete`";
+            }
+
+            if (checkBox7.Checked == true)
+            {
+                query += ", `batch_n_events`";
+            }
+
+            if (checkBox8.Checked == true)
+            {
+                query += ", `batch_comment`";
+            }
+
+            query += " FROM `batches` ";
+            //query = "SELECT `batch_id`, `batch_order_id`, `batch_storage`, `batch_quantity`, `batch_start`, `batch_complete`, `batch_n_events`, `batch_comment` FROM `batches` ";
+
+
+
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string strValue = string.Empty;
+
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+
+            {
+
+                for (int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++)
+
+                {
+
+                    if (!string.IsNullOrEmpty(dataGridView1[j, i].Value.ToString()))
+
+                    {
+
+                        if (j > 0)
+
+                            strValue = strValue + ";" + dataGridView1[j, i].Value.ToString();
+
+                        else
+
+                        {
+
+                            if (string.IsNullOrEmpty(strValue))
+
+                                strValue = dataGridView1[j, i].Value.ToString();
+
+                            else
+
+                                strValue = strValue + Environment.NewLine + dataGridView1[j, i].Value.ToString();
+
+                        }
+
+                    }
+
+                }
+
+                // strValue = strValue + Environment.NewLine;
+
+            }
+
+            string strFile = @"C:\Users\amminex\Desktop\" + textBox1.Text + ".csv";
+            MessageBox.Show("your file have been saved");
+
+
+            if (File.Exists(strFile) && !string.IsNullOrEmpty(strValue))
+
+            {
+                MessageBox.Show("Der eksistere allerede en fil med samme navn.");
+                
+
+            }
+            else
+            {
+                File.Create(strFile).Close();
+                
+                File.WriteAllText(strFile, strValue);
+            }
+
+        }
+
+
+    }
+
+}
+
